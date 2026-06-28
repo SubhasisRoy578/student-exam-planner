b04634ddc935da2c78bca448fb33c1d23f904c4a
Subhasis Roy <roysubhasis32@gmail.com>
2026-06-28 10:09:50 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,10 @@ import { config } from './config/env.js';
 import { prisma } from './config/prisma.js';
 
 const server = app.listen(config.port, () => {
-  // eslint-disable-next-line no-console
   console.log(`Student Exam Planner API listening on port ${config.port} [${config.nodeEnv}]`);
 });
 
 async function shutdown(signal) {
-  // eslint-disable-next-line no-console
   console.log(`${signal} received: closing server gracefully`);
   server.close(async () => {
     await prisma.$disconnect();
